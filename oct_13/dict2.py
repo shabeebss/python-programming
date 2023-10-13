cda6b1286477c71b9a24f85f65cafa614ee82d67
shabeebss <145668237+shabeebss@users.noreply.github.com>
2023-10-13 15:37:54 +0530

    Add files via upload

---
+++ oct_13/dict2.py
@@ -0,0 +1,5 @@
+dic1={1:'a',2:'b',3:'c'}
+dic2={4:'d',5:'e',6:'f'}
+dic3=dic1.copy()
+dic3.update(dic2)
+print(dic3)
---
+++ oct_13/dict5.py
@@ -0,0 +1,5 @@
+dic={1:100,2:200,3:300,4:400,5:500}
+s=0
+for d in dic.values():
+    s=s+d
+print(s)
