9e53dfc13dc16332edf93d6b36d21482b0f4f6ef
shabeebss <145668237+shabeebss@users.noreply.github.com>
2023-10-27 15:54:12 +0530

    3.py

--- oct 19/3.py
+++ oct 19/3.py
@@ -1,3 +1,5 @@
-cl1=["red","green","blue","yellow"]
-cl2=["green","orange","blue"]
-res=[color for
+cl1=["Red","Green","Blue","Yellow"]
+cl2=["Green","Orange","Blue"]
+res=[color for color in cl1 if color not in cl2]
+for color in res:
+    print(color)
