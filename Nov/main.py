1ad821bf0803807cc3946a4687aa8512f6528fba
shabeebss <145668237+shabeebss@users.noreply.github.com>
2023-11-16 14:52:46 +0530

    Create main.py

---
+++ Nov/main.py
@@ -0,0 +1,10 @@
+from mypackage import sum
+from mypackage import avg 
+from mypackage import pow
+
+a=int(input("type first number:"))
+b=int(input("type second number:"))
+print("sum=",sum.sum(a,b))
+print("avg=",avg.avg(a,b))
+print("pow=",pow.pow(a,b))
+
